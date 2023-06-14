86e4000ccbde4bc16cceff2762525a80df0a2dc2
minhduc <minhduc.mll@gmail.com>
2023-06-14 22:27:20 +0700

    Add credentials to axios request

--- src/utils/apiAxios.js
+++ src/utils/apiAxios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const apiRequest = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true,
+    credentials: 'include',
 });
 
 export const cloudinaryUpload = async (file) => {
